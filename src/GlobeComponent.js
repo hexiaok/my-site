c8af4b5590b9d4e503f4e0d711e26fe696cf6b9c
tgmof <>
2025-12-27 22:16:29 +0100

    adding description-long

--- src/GlobeComponent.js
+++ src/GlobeComponent.js
@@ -21,7 +21,8 @@ const GlobeComponent = () => {
         city: 'Zurich',
         country: 'Switzerland',
         year: '2021-now',
-        description: 'a designer @Google',
+        'description-short': 'a designer @Google',
+        'description-long': 'At Google, I began with the beloved Google Flights, and I moved to incubate new commercial verticals, creating a scalable framework that unlocked Google Search’s inaugural booking capabilities. Recently, the work has been a study in connection:driving cross-platform collaborations across Google Search and YouTube.',
         isExpanded: false
       },
       {
@@ -32,7 +33,8 @@ const GlobeComponent = () => {
         city: 'Cambridge',
         country: 'USA',
         year: '2011-2015',
-        description: 'a researcher @GSD',
+        'description-short': 'a researcher @Harvard',
+        'description-long': `At Harvard Exuma Project, the work started in the field: conducting on-site ethnography across the Exuma Islands in Bahamas, documenting local narratives to drive our design decisions. This data found its form in the Exuma Atlas, where I crafted visualizations for the book and website, alongside interactive applications to refine the digital experience.`,
         isExpanded: false
       },
       {
@@ -43,6 +45,10 @@ const GlobeComponent = () => {
         city: 'Ningbo',
         country: 'China',
         year: 'Just a kid',
+        // 'description-short': 'Just a kid',
+        'description-long': `Oh, take their picture," said the woman to her bemused husband, "I think they're artists."
+"oh, go on," he shrugged. "They're just kids."
+ -- Patti Smith "Just Kids"`,
         isExpanded: false
       },
       {
@@ -53,7 +59,8 @@ const GlobeComponent = () => {
         city: 'Berlin',
         country: 'Germany',
         year: '2019-2021',
-        description: 'a designer @DH',
+        'description-short': 'a designer @Delivery Hero',
+        'description-long': 'At Delivery Hero, a platform connecting millions to local food delivery, I worked to bridge the digital and the physical. I designed and launched the pandapay wallet across the APAC region. From there I turned to the last mile delivery experience, collaborating with data scientists to apply machine learning to decipher the delivery notes, ensuring precise and accurate drop-offs.',
         isExpanded: false
       },
       {
@@ -64,7 +71,10 @@ const GlobeComponent = () => {
         city: 'Singapore',
         country: 'Singapore',
         year: '2015-2019',
-        description: 'a designer @Grab',
+        'description-short': 'a designer @Grab, MING Labs',
+        'description-long': `Joined Grab as a founding design member, wearing multiple hats to shape Southeast Asia’s superapp. The work required constant shifting; from field research on driver onboarding to the co-creation workshops that breathed life into GrabFood and GrabRentals.
+
+Moving to MING Labs, a bespoke design agency, I navigated a diverse landscape ranging from niche products to enterprise strategy, leading the research and design for XL Axiata and Asian Development Bank, while developing future-facing concepts for major clients eg. Schneider Electric, Visa.`,
         isExpanded: false
       }
     ];
@@ -114,7 +124,7 @@ const GlobeComponent = () => {
         />
       </div>
       <div className={`hero-section ${isHeroTextVisible ? '' : 'fade-out'}`}>
-                  <div className={`hero-text ${isHeroTextVisible ? 'animate-in' : ''}`}>
+                  <div className={`hero-text ${''/*isHeroTextVisible ? 'animate-in' : ''*/}`}>
                     <span>This</span>&nbsp;<span>is</span><br />
                     <span>Jian</span>&nbsp;<span>He</span>
                   </div>
@@ -122,9 +132,9 @@ const GlobeComponent = () => {
       {showCityDetails && selectedCity && (
         <div ref={cityDetailsRef} className="city-details-section">
           <div className="city-details-title">{selectedCity.year}</div>
-          <div className="city-details-subtitle">{selectedCity.description}</div>
+          <div className="city-details-subtitle">{selectedCity['description-short']}</div>
           <div className="city-details-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis tellus non consequat porta. Integer vehicula vehicula tortor non euismod. Donec vestibulum orci dictum augue vehicula, et aliquam justo malesuada.
+            {selectedCity['description-long']}
           </div>
         </div>
       )}
@@ -133,4 +143,5 @@ const GlobeComponent = () => {
 };
 
 export default GlobeComponent;
+
 
